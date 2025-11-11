8df146dfde738a4827effa81c80840fe3541405c
Ch4120N <charonsecagency@gmail.com>
2025-11-11 09:17:32 -1200

    added Menu function and Updated Banner function

--- ui/banner.py
+++ ui/banner.py
@@ -1,19 +1,40 @@
 # -*- coding: utf-8 -*-
 # core/banner.py
 
+import time
+
 from colorama import Fore, init
 init(autoreset=True)
 
+from ui.numeric_bullets import NumericBulletGenerator
+
 
 def Banner():
-    return fr'''
+    print(fr'''
 {Fore.LIGHTRED_EX}    ___ _     _____                   ___           _     _               
 {Fore.LIGHTRED_EX}   / __\ |__ / _  / ___  _ __   ___  / _ \_ __ __ _| |__ | |__   ___ _ __ 
 {Fore.LIGHTRED_EX}  / /  | '_ \\// / / _ \| '_ \ / _ \/ /_\/ '__/ _` | '_ \| '_ \ / _ \ '__|
 {Fore.LIGHTRED_EX} / /___| | | |/ //\ (_) | | | |  __/ /_\\| | | (_| | |_) | |_) |  __/ |   
 {Fore.LIGHTRED_EX} \____/|_| |_/____/\___/|_| |_|\___\____/|_|  \__,_|_.__/|_.__/ \___|_|
 {Fore.LIGHTRED_EX} 
-{Fore.LIGHTRED_EX}                         {Fore.LIGHTWHITE_EX}Owned By {Fore.LIGHTBLUE_EX}[ {Fore.LIGHTRED_EX}Ch4120N {Fore.LIGHTBLUE_EX}]
-{Fore.LIGHTRED_EX}           {Fore.LIGHTWHITE_EX}GitHub: {Fore.LIGHTGREEN_EX}https://github.com/Ch4120N/ChZoneGrabber
-{Fore.RESET}'''
+{Fore.LIGHTRED_EX}                           {Fore.LIGHTWHITE_EX}Owned By {Fore.LIGHTBLUE_EX}[ {Fore.LIGHTRED_EX}Ch4120N {Fore.LIGHTBLUE_EX}]
+{Fore.LIGHTRED_EX}             {Fore.LIGHTWHITE_EX}GitHub: {Fore.LIGHTGREEN_EX}https://github.com/Ch4120N/ChZoneGrabber
+{Fore.RESET}
+''')
 
+def Menu():
+    bullet_gen = NumericBulletGenerator(prefix=' [ ', brackets_color='bright_red', numbers_color='yellow')
+    time.sleep(0.1)
+    print(bullet_gen.generate('Grab from Archives'))
+    time.sleep(0.1)
+    print(bullet_gen.generate('Grab from Special'))
+    time.sleep(0.1)
+    print(bullet_gen.generate('Grab from Onhold'))
+    time.sleep(0.1)
+    print(bullet_gen.generate('Grab from Notifier'))
+    time.sleep(0.1)
+    print(bullet_gen.generate('All (Archive, Special, Onhold)'))
+    time.sleep(0.1)
+    print(bullet_gen.generate('Settings'))
+    time.sleep(0.1)  
+    print(bullet_gen.generate('Exit'), '\n')

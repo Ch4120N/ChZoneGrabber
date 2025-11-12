6cc293c90430a550d85dbacf48d72ec1e4342d55
Ch4120N <charonsecagency@gmail.com>
2025-11-11 17:31:29 -1200

    Create ChZoneGrabber.py (The main script)

---
+++ ChZoneGrabber.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  ____  _____  _    _  ____  ____  ____  ____     ____  _  _     ___  _   _  __  __  ___   ___  _  _ 
+# (  _ \(  _  )( \/\/ )( ___)(  _ \( ___)(  _ \   (  _ \( \/ )   / __)( )_( )/. |/  )(__ \ / _ \( \( )
+#  )___/ )(_)(  )    (  )__)  )   / )__)  )(_) )   ) _ < \  /   ( (__  ) _ ((_  _))(  / _/( (_) ))  ( 
+# (__)  (_____)(__/\__)(____)(_)\_)(____)(____/   (____/ (__)    \___)(_) (_) (_)(__)(____)\___/(_)\_)
+######################################################################################################
+
+##############################################################
+# ChZoneGrabber : 	Advanced Ch4120N Zone-H Grabber Tool     #
+# Author 	    : 	Ch4120N                                  #
+# Version 	    : 	1.0.0                                    #
+# GitHub 	    : 	https://github.com/Ch4120N/ChZoneGrabber #
+##############################################################
+
+
+import os
+import sys
+import argparse
+
+try:
+    import requests
+    from colorama import Fore, init
+
+    init(autoreset=True)
+except ImportError:
+    sys.exit('\n [ - ] Please install requirements libraries. Using commands below:\n' \
+             '\t- python -m pip install colorama==0.4.6 requests>=2.32.5\n' \
+             '\t- Or\n' \
+             '\t- python -m pip install -r requirements.txt\n'
+            )
+

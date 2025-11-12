91756b1d10d5c5dfef94c1d6e3bebf31f9a37bb5
Ch4120N <charonsecagency@gmail.com>
2025-11-11 17:21:00 -1200

    added main banner

--- ui/banner.py
+++ ui/banner.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 # core/banner.py
 
+from colorama import Fore, init
+init(autoreset=True)
+
+
+def Banner():
+    return fr'''
+{Fore.LIGHTRED_EX}    ___ _     _____                   ___           _     _               
+{Fore.LIGHTRED_EX}   / __\ |__ / _  / ___  _ __   ___  / _ \_ __ __ _| |__ | |__   ___ _ __ 
+{Fore.LIGHTRED_EX}  / /  | '_ \\// / / _ \| '_ \ / _ \/ /_\/ '__/ _` | '_ \| '_ \ / _ \ '__|
+{Fore.LIGHTRED_EX} / /___| | | |/ //\ (_) | | | |  __/ /_\\| | | (_| | |_) | |_) |  __/ |   
+{Fore.LIGHTRED_EX} \____/|_| |_/____/\___/|_| |_|\___\____/|_|  \__,_|_.__/|_.__/ \___|_|
+{Fore.LIGHTRED_EX} 
+{Fore.LIGHTRED_EX}                         {Fore.LIGHTWHITE_EX}Owned By {Fore.LIGHTBLUE_EX}[ {Fore.LIGHTRED_EX}Ch4120N {Fore.LIGHTBLUE_EX}]
+{Fore.LIGHTRED_EX}           {Fore.LIGHTWHITE_EX}GitHub: {Fore.LIGHTGREEN_EX}https://github.com/Ch4120N/ChZoneGrabber
+{Fore.RESET}'''
+

72a4c402b93640b40b6b207d01772363d9412962
Ch4120N <charonsecagency@gmail.com>
2025-11-13 22:26:20 -1200

    Updated write_cfg function

--- core/config_parser.py
+++ core/config_parser.py
@@ -20,7 +20,7 @@ def read_cfg():
     try:
         output_dir = config.get('CONFIG', 'output_dir', fallback='reports/')
         time_date = config.getboolean('CONFIG', 'time_date', fallback=True)
-        max_pages = config.getboolean('CONFIG', 'max_pages', fallback=50)
+        max_pages = config.getint('CONFIG', 'max_pages', fallback=50)
         ZHE_cookie = config.get('ZONE-H CONFIG', 'ZHE', fallback=None)
         PHPSESSID_cookie = config.get('ZONE-H CONFIG', 'PHPSESSID', fallback=None)
     except Exception:
@@ -35,7 +35,7 @@ def read_cfg():
         'PHPSESSID': PHPSESSID_cookie
     }
 
-def write_cfg():
+def write_cfg(max_pages: int = 50, ZHE: str = '', PHPSESSID: str = ''):
     """Create or update a CFG file."""
     _configs = '[CONFIG]\n'
     _configs += ' # The output files are placed inside this directory.\n'
@@ -43,14 +43,14 @@ def write_cfg():
     _configs += ' # The time and date are added after the name of each file.\n'
     _configs += 'time_date = true\n'
     _configs += '# maximum number of pages for crawling\n'
-    _configs += 'max_pages = 50\n\n\n'
+    _configs += f'max_pages = {max_pages}\n\n\n'
     _configs += '# You definitely need to define these two variables in order to be able to do this.\n'
     _configs += '# Read the project\'s documentation file: "README.md" \n'
     _configs += '# Or\n'
     _configs += '# visit this page: https://github.com/Ch4120N/ChZoneGrabber\n'
     _configs += '[ZONE-H CONFIG]\n'
-    _configs += 'ZHE = \n'
-    _configs += 'PHPSESSID = \n'
+    _configs += f'ZHE = {ZHE}\n'
+    _configs += f'PHPSESSID = {PHPSESSID}\n'
 
     with open(Config._config_file, "w") as config_file:
         config_file.write(_configs)
